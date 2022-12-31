b80f6080fd6b050596e924c40255ce43be9eddb2
naveen <112623314+naveen8822@users.noreply.github.com>
2022-12-31 16:37:19 +0530

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,18 @@
+def fun(str1):
+    print(str1)
+    return
+fun("i'm first call to user defined function!")
+fun("again second call to the same function")
+
+
+def printMe(str1,n):
+    n[0]='naveen'
+    print(str1,n)
+    return
+x=['arshad','prathap']
+printMe("Wakeup",x)
+print('x:',x)
+
+
+
+
---
+++ funAdd.py
@@ -0,0 +1,15 @@
+'''
+this module covers 10 th day training of MCA
+'''
+def funAdd(x,y):
+    '''
+this function takes two integer arguments and return sum of it
+ mother theresa institute of computer Applications
+ Naveen MCA
+ 
+ '''
+    temp=x+y
+    return temp
+inpNum1=int(input())
+inpNum2=int(input())
+print(funAdd(inpNum1,inpNum2))
