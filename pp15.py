ac663f228efcd4aab198850e49e3ceec6744412c
naveen <112623314+naveen8822@users.noreply.github.com>
2023-01-06 16:56:38 +0530

    Add files via upload

---
+++ pp15.py
@@ -0,0 +1,11 @@
+message='outer scope'
+def outer():
+    message='demo'
+    print(message)
+    def inner():
+        nonlocal message
+        message ='inner scope'
+        print(message)
+    inner()
+    print(message)
+outer()
